b354242f3cb2a9f2a72415ed5f21bcc8ee6d3be7
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-09-24 17:15:32 -0400

    Adding front end directly through flask

---
+++ backend/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import tensorflow
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/live')
+def live_parse():
+    # import live stream data
+    return render_template('live_parse.html')
+
+@app.route('/archieves')
+def archieves():
+    return render_template('archieves.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080', debug=True)
